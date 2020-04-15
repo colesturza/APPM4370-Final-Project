0d85b8706c908ec2c2b541a9b50d67d8ac43962b
RS-Coop <simpson.r.cooper@gmail.com>
2020-04-15 15:59:41 -0600

    Just some commenting and structural changes.

--- code/FORCE.py
+++ code/FORCE.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.sparse import random
 
 class Force:
-
+    #Set up network parameters and structure
+    #NOTE: Need to implement multiple readouts and inputs
     def __init__(self, *, N=1000, p=0.1, g=1.0, activation=np.tanh, readouts=1):
 
         self.N = N #Number of neurons in the network
@@ -17,23 +18,24 @@ class Force:
         self.W_int = W_int.toarray() #Make it a np matrix
 
         #NOTE: Need to deal with potentially more readouts
-        #NOTE: Shouldnt this be random
+        #NOTE: This could be random, but it might not matter as any kinks will
+        #get fixed once training starts.
         self.W_out = np.zeros(N) #Readout weights
 
-        #Not sure whats going on here
-        self.wf = 2.0*(np.random.rand(N)-0.5)
+        #Feedback weights
+        #NOTE: Check on this initialization
+        self.W_feed = 2.0*(np.random.rand(N)-0.5)
 
-        self.x = 0.5*np.random.randn(N)
-        self.z = 0.5*np.random.randn()
-
+################################################################################
+    #Train the network on specified function
+    #NOTE: Need to implement multiple readuts and inputs
     def fit(self, func_to_learn, nsecs, *, alpha=1.0, dt=0.1, learn_every=2):
-
-        #Setting up some stuff
-
+    #Setting up some stuff
         dW_out = np.zeros(self.N) #Weight update vector
 
         #Simulation time and length of that vector
-        #NOTE: What if we aren't learning time dependent function
+        #NOTE: What if we aren't learning time dependent function.
+        #What if we are learning some input dependent function
         simtime = np.arange(0, nsecs, dt)
         simtime_len = len(simtime)
 
@@ -45,46 +47,58 @@ class Force:
 
         zt = np.zeros(simtime_len) #Essentially the output function (vector)
 
-        #Okay so now we are leanrning
+    #Okay so now we are leanrning
+        #x is pre-activation and z is readout
+        #NOTE: Check in to these
+        x = 0.5*np.random.randn(N)
+        z = 0.5*np.random.randn()
 
-        #Not sure about this
-        #NOTE: Why are we calculating this here if we recalculate a few lines down
-        #Response to NOTE: It's because we need r in line 59, then recalculate r with new x
-        r = self.activation(self.x)
+        #post-activation
+        #NOTE: Could calculate this from parameters
+        r = self.activation(x)
 
         P = (1.0/alpha)*np.eye(self.N) #Inverse correlation matrix
 
+        #Iterate and train the network
         for ti, t in enumerate(simtime):
             # sim, so x(t) and r(t) are created.
-            self.x = (1.0-dt)*self.x + self.W_int.dot(r*dt) + self.wf*(self.z*dt)
-            r = self.activation(self.x)
-            self.z = self.W_out.dot(r)
+            #NOTE: Check in to this stuff
+            x = (1.0-dt)*x + self.W_int.dot(r*dt) + self.W_feed*(z*dt)
+            r = self.activation(x)
+            z = self.W_out.dot(r)
 
             if (ti+1) % learn_every == 0:
-                # update inverse correlation matrix
+                #Update inverse correlation matrix
                 k = P.dot(r)
                 rPr = r.dot(k)
                 c = 1.0/(1.0 + rPr)
                 P = P - np.outer(k, k * c)
 
-                # update the error for the linear readout
-                e = self.z - ft[ti]
+                #Update the error for the linear readout
+                e = z - ft[ti]
 
-                # update the output weights
+                #Update the output weights
                 dW_out = -e * k * c
-
                 self.W_out = self.W_out + dW_out
 
-            # Store the output of the system.
-            zt[ti] = self.z
+            #Store the output of the system.
+            zt[ti] = z
+
+            #Magnitude of weights? Whats going on here
+            #NOTE: Look into this
             wo_len[ti] = np.sqrt(self.W_out.dot(self.W_out))
 
+        #Average error after learning
         error_avg = np.sum(np.abs(zt-ft))/simtime_len
         print('Training MAE: {:.3f}'.format(error_avg))
 
+        #Return the training progression I think
+        #NOTE: Look into this
         return zt, wo_len
 
-
+################################################################################
+    #Use the trained neural network predict or generate
+    #NOTE: Need to consider multiple readouts and inputs
     def predict(self, start, end, dt):
 
         simtime = np.arange(start, end, dt)
@@ -99,7 +113,7 @@ class Force:
         for ti, t in enumerate(simtime):
 
             # sim, so x(t) and r(t) are created.
-            x = (1.0-dt)*x + self.W_int.dot(r*dt) + self.wf*(z*dt)
+            x = (1.0-dt)*x + self.W_int.dot(r*dt) + self.W_feed*(z*dt)
             r = self.activation(x)
             z = self.W_out.dot(r)
 
@@ -107,6 +121,9 @@ class Force:
 
         return t, zpt
 
+################################################################################
+    #Evaluate the neural network
+    #NOTE: Need to consider multiple readouts and inputs
     def evaluate(self, start, end, dt, func_learned):
 
         t, zpt = self.predict(start, end, dt)
