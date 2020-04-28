f87c2c48471ffd5160ca67d0e9ffc762dcad252b
Cole Sturza <colesturza@gmail.com>
2020-04-28 13:37:58 -0600

    PCA for 2 and 3 outputs

--- code/modules/PCA.py
+++ code/modules/PCA.py
@@ -3,7 +3,11 @@ import numpy as np
 from scipy.sparse import random
 from sklearn import preprocessing
 
-def PCA(data, n_components=8, return_matrix=True, return_eigenvalues=True):
+# Perform PCA on a given set of time series data
+# n_components = the number of leading principal components to keep
+# return_proj = return projection of data points over the eigenvectors if True
+# return_eigenvals = return leading principal components' eigenvalue if True
+def PCA(data, *, n_components=8, return_proj=True, return_eigenvals=True):
 
     # Covariance matrix
     cov_matrix = np.cov(preprocessing.scale(data.T))
@@ -11,19 +15,23 @@ def PCA(data, n_components=8, return_matrix=True, return_eigenvalues=True):
     # Diagonalization of the covariance matrix
     eig_val, eig_vec = np.linalg.eigh(cov_matrix)
 
-    if return_matrix or return_eigenvalues:
+    if return_proj or return_eigenvals:
 
-        if return_matrix:
+        if return_proj:
+
             # Projection of the data points over the eigenvectors
             Proj = data.dot(eig_vec[:,-n_components:])
 
-        if return_matrix and return_eigenvalues:
-            return eig_vec[:,-n_components:], Proj, eig_val
+            if return_eigenvals:
 
-        elif return_matrix:
-            return eig_vec[:,-n_components:], Proj
+                return eig_vec[:,-n_components:], Proj, eig_val
+
+            else:
+
+                return eig_vec[:,-n_components:], Proj
 
         else:
+
             return eig_vec[:,-n_components:], eig_val
 
     return eig_vec[:,-n_components:]
@@ -31,12 +39,14 @@ def PCA(data, n_components=8, return_matrix=True, return_eigenvalues=True):
 class PCA_Network(Force):
 
 ################################################################################
-    def fit(self, simtime, func_to_learn, *, alpha=1.0, n_components=8):
+#Use the trained neural network predict or generate
+#NOTE: Need to consider multiple readouts and inputs
+    def predict(self, simtime, n_components=8):
 
-    #Setting up some stuff
+        if self.x is None:
+            x = self.setIC()
+        else: x = self.x
 
-        #NOTE: I suppose we are only learning time dependent funcs
-        #Simulation time and length of that vector
         simtime_len = simtime.shape[0]
 
         diff = np.diff(simtime)
@@ -45,86 +55,70 @@ class PCA_Network(Force):
 
         dt = diff[0]
 
-        #Weight update vector
-        dW_out = np.zeros((self.N, self.readouts))
+        if self.saveInternal:
+            self.intOut = np.zeros((simtime_len, self.num2save))
 
-        #Check if func_to_learn is either a callable function or an
-        #ndarray of values to learn.
-        if callable(func_to_learn):
-            ft = func_to_learn(simtime) #Function being learned (vector)
-        elif type(func_to_learn) is np.ndarray:
-            ft = func_to_learn #Input is an array
-        else:
-            raise ValueError("""func_to_learn must either be a callable function
-                or a numpy ndarray of shape (n, {}).""".format(self.readouts))
+        zpt = np.zeros((simtime_len, self.readouts))
 
-        ft = ft.reshape((simtime_len, 1))
-
-        #Essentially the output function (vector)
-        zt = np.zeros((simtime_len, self.readouts))
-
-    #Okay so now we are leanrning
-
-        #x is pre-activation and z is readout
-        #x has to be initialized somewhere
-        #NOTE: Why is z random as well
-        if self.x is None:
-            x = self.setIC()
-        else: x = self.x
-
-        #post-activation
         r = self.activation(x)
-
-        if self.rand_z:
-            z = 0.5*np.random.randn(self.readouts, 1)
-        else: z = self.W_out.T.dot(r)
-
-        P = (1.0/alpha)*np.eye(self.N) #Inverse correlation matrix
-
-        #Magnitude of weights as we learn
-        proj_ws = np.zeros((simtime_len, n_components, self.readouts))
+        z = self.W_out.T.dot(r)
 
         # Store x after activation (r) at each time step
         X = np.zeros((simtime_len, self.N))
 
-        #Iterate and train the network
         for ti in range(simtime_len):
 
+            # sim, so x(t) and r(t) are created.
+            # x = ((1.0-dt)*x + self.W_int.dot(r*dt) + self.W_feed.dot(z)*dt)/self.tau
             x = x - (x*dt + self.W_int.dot(r*dt) + self.W_feed.dot(z)*dt)/self.tau
             r = self.activation(x)
             z = self.W_out.T.dot(r)
 
+            if self.saveInternal:
+                self.intOut[ti,:] = r[:self.num2save,0]
+
             #Store r at time step ti
             X[ti, :] = r.reshape(self.N)
 
-            #Update inverse correlation matrix
-            k = P.dot(r)
-            rPr = r.T.dot(k)
-            c = 1.0/(1.0 + rPr)
-            P = P - np.outer(k, k * c)
-
-            #Update the error for the linear readout
-            e = z - ft[ti].reshape((self.readouts, 1))
-
-            #Update the output weights
-            dW_out = -k.dot(e.T) * c
-            self.W_out = self.W_out + dW_out
-            #
-            # if ti%100 == 0 and ti != 0:
-            #     print('Finished {}'.format(ti))
-
-        self.x = x #Keep the final state
-
-        eigvecs, proj, eigvals = PCA(X, n_components=n_components, return_matrix=True, return_eigenvalues=True)
+        eigvecs, proj, eigvals = PCA(X, n_components=n_components, return_proj=True, return_eigenvals=True)
 
         # Projection over the leading principal components
-        proj_w = self.W_out.T.dot(eigvecs)
+        proj_w = self.W_out.T.dot(eigvecs) # proj_w readouts x pc components kept
 
+        # z_proj simtime_len x readouts
         z_proj = proj.dot(proj_w.T)
 
         eigvals = eigvals[::-1]
 
         proj = proj.T[::-1,...]
 
-        #Return the training progression
-        return z_proj, eigvals, proj, proj_ws
+        return z_proj, eigvals, proj
+
+################################################################################
+    #Evaluate the neural network
+    #NOTE: Need to consider multiple readouts and inputs
+    #NOTE: Should check on all of this stuff
+    def evaluate(self, x, simtime, func_learned):
+
+        z_proj, _, _ = self.predict(x, simtime)
+
+        #NOTE: I suppose we are only learning time dependent funcs
+        #Simulation time and length of that vector
+        simtime_len = simtime.shape[0]
+
+        #Check if func_to_learn is either a callable function or an
+        #ndarray of values to learn.
+        if callable(func_learned):
+            ft = func_learned(simtime) #Function being learned (vector)
+        elif type(func_learned) is np.ndarray:
+            ft = func_learned #Input is an array
+        else:
+            raise ValueError("""func_learned must either be a callable function
+                or a numpy ndarray of shape (n, {}).""".format(self.readouts))
+
+        ft = ft.reshape((simtime_len, self.readouts))
+
+        error_avg = np.sum(np.abs(np.subtract(z_proj, ft)))/simtime_len
+        print('Testing MAE: {:.5f}'.format(error_avg))
+
+        return error_avg
